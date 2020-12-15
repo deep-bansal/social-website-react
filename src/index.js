2cb07f9b86a1692f4bb02d35d5059b39c6c4009f
deep-bansal <deepvinaybansal@yahoo.com>
2020-12-15 15:26:23 +0530

    add ui prop-types and navbar

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import { configureStore } from './store/index';
+import { Provider } from 'react-redux';
 
 const store = configureStore();
 console.log('store', store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+
   document.getElementById('root')
 );
