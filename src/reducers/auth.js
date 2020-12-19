ec75f4e709be72b0d2202ea1e6544c44f12bca36
deep-bansal <deepvinaybansal@yahoo.com>
2020-12-19 11:50:08 +0530

    user loggin -1

---
+++ src/reducers/auth.js
@@ -0,0 +1,30 @@
+import { LOGIN_START, LOGIN_SUCCESS } from '../actions/actionTypes';
+
+const initialAuthState = {
+  user: {},
+  isLoggedIn: false,
+  inProgress: false,
+  error: null,
+};
+export default function auth(state = initialAuthState, action) {
+  switch (action.type) {
+    case LOGIN_START:
+      return {
+        ...state,
+        inProgress: true,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        inProgress: false,
+        isLoggedIn: true,
+        user: action.user,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        inProgress: false,
+        error: action.error,
+      };
+  }
+}
